bd007dce60a791e246728f5a0ae2e00b0b819c6a
Jonas <JNSAPH@icloud.com>
2022-12-28 14:58:45 +0100

    Initial Commit

---
+++ search/variables_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['m_5fsockets_0',['m_sockets',['../class_web_socket_server.html#a3caffa4eb6cfb366dfcd372ec451e434',1,'WebSocketServer']]],
+  ['m_5fsocketserver_1',['m_socketServer',['../class_web_socket_server.html#ac4656d2bdcd447b50ccbccf85eb68fca',1,'WebSocketServer']]]
+];
